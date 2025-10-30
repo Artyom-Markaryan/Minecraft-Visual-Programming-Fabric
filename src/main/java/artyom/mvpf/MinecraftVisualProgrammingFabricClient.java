45a32f6fb0bf42c0ad8068390e31186ca636009c
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-10-29 23:20:23 -0400

    Configuré le projet et les librairies

---
+++ src/main/java/artyom/mvpf/MinecraftVisualProgrammingFabricClient.java
@@ -0,0 +1,8 @@
+package artyom.mvpf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftVisualProgrammingFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
